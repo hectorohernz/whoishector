e7cd12d2f9110d364a63c0ed745b9fd8dc3c3314
Hector Hernandez <hectorohernz@gmail.com>
2020-06-15 15:30:05 -0400

    fixing styles

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import data from '../db/Content.json';
+import { v4 as uuidv4 } from 'uuid';
 export default class Portfolio extends Component{
     constructor(props){
         super(props);
@@ -18,6 +19,10 @@ export default class Portfolio extends Component{
                             <h3>{project.title}</h3>
                             <a href={project.link} target="_blank" rel="noopener noreferrer">Project Link</a>
                             <p>{project.description}</p>
+                            <h4>Technologies Used</h4>
+                            <ul>
+                                {project.tags.map(item => <li key={uuidv4()}>{item}</li>)}
+                            </ul>
                         </div>
                     )}
 
