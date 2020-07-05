e11658d7781aabac8047420f3ec06ab516475b6d
Hector Hernandez <hectorohernz@gmail.com>
2020-07-05 10:22:00 -0400

    fix react router issues

--- src/App.js
+++ src/App.js
@@ -2,23 +2,25 @@ import React from 'react';
 import './App.css';
 import Intro from './components/Intro';
 import Nav from './components/Nav'
-import {Route, Switch} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Portfolio from './components/Portfolio';
 import Contact from './components/Contact';
 import Soon from './components/Soon'
 class App extends React.Component{
   render() {
     return (
+      <Router>
       <main className="main-app">
         <Nav/>
       <Switch>
-        <Route exact render={ () =>  <Intro/>} path="/"/>
-        <Route exact render={ () =>  <Portfolio />} path="/portfolio"/>
-        <Route exact render={ () =>  <Soon />} path="/contact"/>
-        <Route exact render={ () =>  <Soon />} path="/graphic"/>
-        <Route exact render={ () =>  <Soon />} path="/aboutme"/>
+        <Route exact component={Intro} path="/"/>
+        <Route  component={  Portfolio} path="/portfolio"/>
+        <Route  component={ Soon } path="/contact"/>
+        <Route  component={ Soon } path="/graphic"/>
+        <Route  component={Soon } path="/aboutme"/>
       </Switch>
       </main>
+      </Router>
     )
   }
 }
