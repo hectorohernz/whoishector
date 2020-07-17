2743f5a192018a171b0ae7f753c0801489157268
Hector Hernandez <hectorohernz@gmail.com>
2020-07-16 20:59:42 -0400

    adding final touches

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,11 +6,12 @@ export default class contact extends Component {
             <>
                 <section className="contact-page-container">
                     <h1>Contact Me</h1>
-                    <form name="contact" method='post'>
-                        s<input type="hidden" name="form-name" value="contact" />
-                        <input type="text" name="name" />
-                        <input type="email" name="email" />
-                        <textarea name="message"></textarea>
+                    <form name="contact" className="form-container"  method='post'>
+                        <input type="hidden"  name="form-name" value="contact" />
+                        <input type="text"  placeholder="Full Name" className="text-container" name="name" />
+                        <input type="email"  placeholder="Email" className="text-container" name="email" />
+                        <textarea   placeholder="Message" className="text-area"  name="message"></textarea>
+                        <input type="submit" value="Send"/>
                     </form>
                 </section>
                 
