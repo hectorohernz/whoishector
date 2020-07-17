263c79f3df50166e46e27adbd411ef24e90c2d1b
Hector Hernandez <hectorohernz@gmail.com>
2020-07-16 20:53:47 -0400

    contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
 
-export default class contact extends Component{
+export default class contact extends Component {
     render() {
         return (
             <>
                 <section className="contact-page-container">
                     <h1>Contact Me</h1>
-                        <form name="contact" data-netlify="true" method="/" className="form-container">
-                            <input  className="text-container" type="text"  id="name" placeholder="Full Name" name="name"/>
-                            <input  className="text-container"  type="email"  id="email" placeholder="Email" name="email"/>
-                            <textarea className="text-area" placeholder="Message" name="message" rows="4" cols="50"></textarea>
-                            <input type="submit" value="Send"/>
-                        </form>
+                    <form name="contact" method='post'>
+                        s<input type="hidden" name="form-name" value="contact" />
+                        <input type="text" name="name" />
+                        <input type="email" name="email" />
+                        <textarea name="message"></textarea>
+                    </form>
                 </section>
+                
             </>
         )
     }
