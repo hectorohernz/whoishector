a35b4238968fc97554c3bc31388280f2a8084197
Hector Hernandez <hectorohernz@gmail.com>
2020-07-16 20:40:02 -0400

    contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,7 +6,7 @@ export default class contact extends Component{
             <>
                 <section className="contact-page-container">
                     <h1>Contact Me</h1>
-                        <form data-netlify="true" method="/" className="form-container">
+                        <form name="contact" data-netlify="true" method="/" className="form-container">
                             <input  className="text-container" type="text"  id="name" placeholder="Full Name" name="name"/>
                             <input  className="text-container"  type="email"  id="email" placeholder="Email" name="email"/>
                             <textarea className="text-area" placeholder="Message" name="message" rows="4" cols="50"></textarea>
