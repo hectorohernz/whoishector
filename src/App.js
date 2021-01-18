a19360f91adbda2a214f833f4496f0acd0822237
highsgod <hectorohernz@gmail.com>
2021-01-18 17:08:01 -0500

    New Design

--- src/App.js
+++ src/App.js
@@ -1,34 +1,19 @@
-import React from 'react';
-import './App.css';
-import Intro from './components/Intro';
-import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import Portfolio from './components/Portfolio';
-import Contact from './components/Contact';
-import Soon from './components/Soon';
-import nonExist from './components/404';
-import aboutMe from './components/Aboutme';
-import Project from './components/projectModel';
-import Footer from './components/Footer';
-import PlineNav from './pipeline-1/Nav';
+import React from "react";
+import "./styles/main.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./components/Home";
 
-class App extends React.Component{
+class App extends React.Component {
   render() {
     return (
       <Router>
-     <PlineNav/>
-      <main className="main-app">
-      <Switch>
-        <Route exact component={Intro} path="/"/>
-        <Route  component={  nonExist} path="/portfolio"/>
-        <Route  component={ Contact } path="/contact"/>
-        <Route  component={ Soon } path="/graphic"/>
-        <Route  component={aboutMe } path="/aboutme"/>
-        <Route exact path="/:projectId" component={Project} />
-      </Switch>
-      </main>
-      <Footer/>
+        <main className="main-app">
+          <Switch>
+            <Route exact component={Home} path="/" />
+          </Switch>
+        </main>
       </Router>
-    )
+    );
   }
 }
 
---
+++ src/components/Home.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+export default class Home extends Component{
+   render() {
+       return (
+                <section className="home">
+                    <div className="home--title">
+                        <p>Hi, My name is</p>
+                        <h1>Hector Hernandez.</h1>
+                        <h2>I build things for the web.</h2>
+                    </div>
+                    <div className="home--description">
+                        <p>I'm an aspiring full stack <b>Software Engineer</b> based in <b>Charlotte, NC</b>, specializing in developing quality websites and applications. Currently working at <b>Bank of America</b> as a <b>Front End Engineer.</b> </p>
+                        <p>I love hiking, traveling, cooking, working on side projects, and learning new things.</p>
+                    </div>
+                    <div className="home--links">
+                        <h3>Get In Touch</h3>
+                        <ul>
+                            <li><a href="https://github.com/hectorohernz" target="_blank" rel="noopener noreferrer">Github</a></li>
+                            <li><a href="https://linkedin.com/in/hector-hernandez-383444196" rel="noopener noreferrer" target="_blank">Linkedin</a></li>
+                            <li><a href="mailto:hectorohernz@gmail.com" target="_blank" rel="noopener noreferrer">Email Me</a></li>
+                        </ul>
+                    </div>
+                    <p className="quote">True Love Will Find You In The End.</p>
+                </section>
+       )
+   }
+}
